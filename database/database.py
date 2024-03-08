cbbb9aa4a161d0d0c8e5898dc52e96e717f1525b
Kavindu Fernando <kavindufernando329@gmail.com>
2024-03-09 00:56:14 +0530

    Delete unused files and code

--- database/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URI = "sqlite:///alieat.sqlite"
-engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
